731678d816179108bb30063d45cd59631ae184ce
wogembo <wycliff.ogembo@swiftcel.com>
2017-08-27 20:13:24 +0300

    Made pympesa pip-installable

---
+++ pympesa/__init__.py
@@ -0,0 +1,2 @@
+def project():
+    return "Pympesa"
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pympesa',
+      version='0.1',
+      description='Mpesa rest api library',
+      url='https://bitbucket.org/pythias_io/pympesa',
+      author='Wycliff Ogembo',
+      author_email='wycliffogembo87@gmail.com',
+      license='MIT',
+      packages=['pympesa'],
+      zip_safe=False)

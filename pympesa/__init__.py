e1b90b923821bac118d8fbb74df6bdfaf589c7aa
wogembo <wycliff.ogembo@swiftcel.com>
2017-08-28 19:51:10 +0300

    Added expected payload parameters

--- pympesa/__init__.py
+++ pympesa/__init__.py
@@ -5,6 +5,11 @@
 This exposes various mpesa functionalities to developers.
 """
 
+from datetime import datetime
+
+authorization = "Bearer <Access-Token>"
+content_type = "application/json"
+
 consumer_key = "4318nVqAGpz2skVYvy47B3PLDpuSARD8"
 consumer_secret = "atXr69HNm4LQ0RBH"
 
@@ -38,36 +43,190 @@ authorization_basic = ("SjhYNFdVQUsyc3ZjWHpiNlNHd1hyNmpSV" +
 access_token = "bQ6UHJnO2Xqn0ieOsVACaTUUUvCB"
 
 
-def b2b():
+def b2b_payment_request():
     """Mpesa Transaction from one company to another."""
-    pass
+
+    url = "https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest"
+
+    payload = {
+        "Initiator": "",
+        "SecurityCredential": "",
+        "CommandID": "",
+        "SenderIdentifierType": "",
+        "RecieverIdentifierType": "",
+        "Amount": "",
+        "PartyA": "",
+        "PartyB": "",
+        "AccountReference": "",
+        "Remarks": "",
+        "QueueTimeOutURL": "",
+        "ResultURL": ""
+    }
+
+    return payload
 
 
-def b2c():
+def b2c_payment_request():
     """Mpesa Transaction from company to client."""
-    pass
+
+    url = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest"
+
+    payload = {
+        "InitiatorName": "",
+        "SecurityCredential": "",
+        "CommandID": "",
+        "Amount": "",
+        "PartyA": "",
+        "PartyB": "",
+        "Remarks": "",
+        "QueueTimeOutURL": "",
+        "ResultURL": "",
+        "Occassion": ""
+    }
+
+    return payload
 
 
-def c2b():
-    """Register URL for Validation/Confirmation and Simulate transaction."""
-    pass
+def c2b_register_url():
+    """Use this API to register validation and confirmation URLs on M-Pesa."""
+
+    url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+
+    payload = {
+        "ShortCode": "",
+        "ResponseType": "",
+        "ConfirmationURL": "",
+        "ValidationURL": ""
+    }
+
+    return payload
+
+
+def c2b_simulate_transaction():
+    """Use this API to simulate a C2B transaction."""
+
+    url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate"
+
+    payload = {
+        "ShortCode": "",
+        "CommandID": "CustomerPayBillOnline",
+        "Amount": "",
+        "Msisdn": "",
+        "BillRefNumber": ""
+    }
+
+    return payload
 
 
 def transation_status():
     """Use this API to check the status of transaction."""
-    pass
+
+    url = "https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query"
+
+    payload = {
+        "Initiator": "",
+        "SecurityCredential": "",
+        "CommandID": "TransactionStatusQuery",
+        "TransactionID": "",
+        "PartyA": "",
+        "IdentifierType": "1",
+        "ResultURL": "",
+        "QueueTimeOutURL": "",
+        "Remarks": "",
+        "Occasion": ""
+    }
+
+    return payload
 
 
 def account_balance():
     """Use this API for reversal transaction."""
-    pass
+
+    url = "https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query"
+
+    payload = {
+        "Initiator": "",
+        "SecurityCredential": "",
+        "CommandID": "AccountBalance",
+        "PartyA": "",
+        "IdentifierType": "4",
+        "Remarks": "",
+        "QueueTimeOutURL": "",
+        "ResultURL": ""
+    }
+
+    return payload
 
 
 def reversal():
     """Use this API for reversal transaction."""
+
+    url = "https://sandbox.safaricom.co.ke/mpesa/reversal/v1/request"
+
+    payload = {
+        "Initiator": "",
+        "SecurityCredential": "",
+        "CommandID": "TransactionReversal",
+        "TransactionID": "",
+        "Amount": "",
+        "ReceiverParty": "",
+        "RecieverIdentifierType": "4",
+        "ResultURL": "",
+        "QueueTimeOutURL": "",
+        "Remarks": "",
+        "Occasion": ""
+    }
+
+    return payload
+
+
+def lipa_na_mpesa_online_query():
+    """For Lipa Na M-Pesa online payment using STK Push.
+
+    Use this API to check the status of a Lipa Na M-Pesa Online Payment."""
+
+    url = "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query"
+
+    payload = {
+        "BusinessShortCode": " ",
+        "Password": " ",
+        "Timestamp": " ",
+        "CheckoutRequestID": " "
+    }
+
+    return payload
+
+
+def lipa_na_mpesa_online_payment():
+    """For Lipa Na M-Pesa online payment using STK Push.
+
+    Use this API to initiate online payment on behalf of a customer."""
+
+    url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+
+    payload = {
+        "BusinessShortCode": "",
+        "Password": "",
+        "Timestamp": "",
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": "",
+        "PartyA": "",
+        "PartyB": "",
+        "PhoneNumber": "",
+        "CallBackURL": "",
+        "AccountReference": "",
+        "TransactionDesc": ""
+    }
+
+    return payload
+
+
+def generate_oauth_token():
+    """Gives you time bound access token to call allowed APIs."""
+    # url = ("https://sandbox.safaricom.co.ke/oauth/v1/generate?" +
+    #        "grant_type=client_credentials")
     pass
 
 
-def lipa_na_mpesa_online():
-    """For Lipa Na M-Pesa online payment using STK Push."""
-    pass
+def generate_timestamp():
+    return datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")

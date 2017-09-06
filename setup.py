66c3255c0d9c827ac0fcd07ccf53a32b1da1200a
wogembo <wycliff.ogembo@swiftcel.com>
2017-09-06 17:21:31 +0300

    Updated pypi package version to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pympesa',
-      version='0.2',
+      version='0.3',
       description='Mpesa rest api library',
       url='https://bitbucket.org/pythias_io/pympesa',
       author='Andrew Kamau',

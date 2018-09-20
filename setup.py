1e58580457b9ae7537299d625fc8ab941c2c34dc
andrew kamau <andkamau@gmail.com>
2018-09-20 14:25:14 +0300

    bump up version no.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='pympesa',
-      version='1.0',
-      description='Mpesa rest api library',
+      version='1.0.2',
+      description='Daraja (Mpesa REST API) python library',
       url='https://github.com/pythias-io/pympesa',
       author='Andrew Kamau',
       author_email='andrew@pythias.io',
